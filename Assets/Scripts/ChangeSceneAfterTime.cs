226543b328c70c47cfe457708c0c6b91946f5038
Bill Spooner <bill.s@livetechgames.com>
2022-12-20 14:19:31 +0000

    add title scene transition script

--- Assets/Scripts/ChangeSceneAfterTime.cs
+++ Assets/Scripts/ChangeSceneAfterTime.cs
@@ -1,11 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeSceneAfterTime : MonoBehaviour
 {
     public string sceneName;
     public float waitTimeSeconds;
+
+    private float _initial;
     
-    
+    private void Start()
+    {
+        _initial = Time.realtimeSinceStartup;
+    }
+
+    private void Update()
+    {
+        if (Time.realtimeSinceStartup > _initial + waitTimeSeconds)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
 }
