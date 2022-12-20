c57ff59c0e96d72dfcdd9a69b7f7afea3cac929a
James Foward <james.f@livetechgames.com>
2022-12-20 00:59:19 +0000

    Intro and a trigger

--- Assets/Scripts/HurtTrigger.cs
+++ Assets/Scripts/HurtTrigger.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class HurtTrigger : MonoBehaviour
@@ -9,9 +7,9 @@ public class HurtTrigger : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Bread") || other.gameObject.CompareTag("Duck"))
         {
-            Health health = other.gameObject.GetComponent<Health>(); 
+            Health health = other.gameObject.GetComponentInParent<Health>(); 
             health.TakeDamage();
-            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            Rigidbody2D rb = other.gameObject.GetComponentInParent<Rigidbody2D>();
             rb.AddForce(transform.up * 500);
         }
     }
