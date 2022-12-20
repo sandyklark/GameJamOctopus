bb27b3721b885cf73531e271efd7303c95b65000
Bill Spooner <bill.s@livetechgames.com>
2022-12-20 14:15:15 +0000

    update title

---
+++ Assets/Scripts/ChangeSceneAfterTime.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneAfterTime : MonoBehaviour
+{
+    public string sceneName;
+    public float waitTimeSeconds;
+    
+    
+}
